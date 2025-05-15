ce9b8fc40d311b29df840f45e1e3993b26367c5c
Mauro <mausrosales@gmail.com>
2025-05-15 16:45:00 -0300

    Agregue la consigna de parcialito-2

--- parcialito.py
+++ parcialito.py
@@ -1,3 +1,29 @@
+"""
+    Objetivos de Aprobación Directa (Calificación de 6 a 10 puntos):
+
+Realizar el juego de la Batalla Naval
+Dado el siguiente tablero con los barcos ubicados de la siguiente forma:
+tablero = [ 
+            [0, 0, 1, 0, 0],
+            [0, 1, 0, 1, 0],
+            [1, 0, 0, 1, 0],
+            [1, 0, 0, 0, 1]]
+
+Pedirle una coordenada al usuario para la fila (x) [validar 0-4]
+Pedirle una coordenada al usuario para la columna (y) [validar 0-4]
+
+Desarrollar una función con el siguiente prototipo:
+    verificar_coordenadas(tablero: list, x: int, y: int) -> bool:
+La función debe retornar "True" si el barco fue hundido (1) y "False" si disparó en el agua (0)
+Según lo retornado por la función, mostrar al usuario los siguientes mensajes:
+    "Hundido"   "Agua"
+
+El usuario puede realizar tantos disparos como quiera, cuando no quiera seguir disparando, se le debe informar cuántos barcos fueron hundidos.
+
+Nota: No se pueden utilizar funciones propias.
+"""
+
+
 
 tablero = [
         [0, 0, 1, 0, 0],
@@ -37,5 +63,5 @@ def preguntar_batalla():
         print(f"\n barcos hundidos: {contador}\n")
     
 
-
-preguntar_batalla()
+if __name__ == "__main__":
+    preguntar_batalla()
